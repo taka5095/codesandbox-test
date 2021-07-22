039b6e52429b7feee16035a389a4eac53b87b2b8
taka5095 <87805831+taka5095@users.noreply.github.com>
2021-07-22 22:30:26 +0900

    const,letの解説

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-import "./styles.css";
+/**
+ * const,let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var var1 = "var変数";
+// console.log(var1);
+
+// //var変数は上書き可能
+// var1 = "var変数を上書き"
+// console.log(var1);
+
+// //var変数は再宣言可能
+// var var1 = "var変素を再宣言"
+// console.log(var1);
+
+// let var2 = "let変数"
+// console.log(var2);
+
+// //letは上書き可能
+// var2 = "let変数を上書き"
+// console.log(var2);
+
+// //letは再宣言不可
+// let var2 = "let変数を再宣言"
+// console.log(var2);
+
+// const var3 = "const変数"
+// console.log(var3);
+
+// //const変素は上書き不可
+// var3 = "const変数を上書き"
+// console.log(var3);
+
+// //const変素は再宣言不可
+// const var3 = "const変数を再宣言"
+// console.log(var3);
+
+//constの場合も、objectの場合はプロパティの変更が可能
+// const var4 = {
+//   name: "宮田",
+//   age: 27,
+// };
+//  var4.name = "みやた"
+//  var4.address = "Tokyo"
+//  console.log(var4);
+
+//constの場合も、配列の場合は中身の変更が可能
+const var5 = ["dog", "cat"];
+var5[0] = "bird";
+var5.push("monkey");
+console.log(var5);
